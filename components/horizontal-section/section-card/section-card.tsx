f57208989f15c0475a32af3198350d4749496a65
Filipo <filipfilip280@gmail.com>
2024-07-26 01:00:11 +0200

    feat: improve audio player view

--- components/horizontal-section/section-card/section-card.tsx
+++ components/horizontal-section/section-card/section-card.tsx
@@ -1,11 +1,11 @@
-import { SongsRecord } from "@/pocketbase-types";
+import { SongsRecord, SongsTypeOptions } from "@/pocketbase-types";
 import { StyleProp, View, ViewStyle } from "react-native";
 import { SectionCardContainer, SectionCardTitle } from "./section-card.shared";
 import TrackPlayer, {
   useActiveTrack,
   useIsPlaying,
 } from "react-native-track-player";
-import { Card, IconButton } from "react-native-paper";
+import { Badge, Card, IconButton } from "react-native-paper";
 import { pb } from "@/utils/pocketbase";
 
 export function BaseSectionCard(props: {
@@ -13,7 +13,7 @@ export function BaseSectionCard(props: {
   imageUrl: string;
   id: string;
   style?: StyleProp<ViewStyle>;
-  href?: string
+  href?: string;
 }) {
   return (
     <SectionCardContainer href={props.href} style={[props.style]}>
@@ -48,44 +48,58 @@ export function SongSectionCard(props: {
 }) {
   const track = useActiveTrack();
   const { playing, bufferingDuringPlay } = useIsPlaying();
+  const songUrl = pb.files.getUrl(props.song, props.song.file ?? "");
+  const isActiveTrack = songUrl === track?.url;
 
   return (
     <SectionCardContainer href={`/songs/${props.id}`} style={[props.style]}>
       <View style={{ position: "relative" }}>
-        <Card.Cover source={{ uri: props.imageUrl }} />
+        <View style={{ position: "relative" }}>
+          <Card.Cover source={{ uri: props.imageUrl }} />
+          {props.song.type !== SongsTypeOptions.orginal && (
+            <Badge style={{ position: "absolute", top: 5, left: 5 }}>
+              {props.song.type}
+            </Badge>
+          )}
+        </View>
 
-        <IconButton
-          style={{ position: "absolute", bottom: 2, right: 2, zIndex: 50 }}
-          icon={getPlayerIcon({
-            bufferingDuringPlay,
-            playing,
-            isCurrent:
-              pb.files.getUrl(props.song, props.song.file ?? "") === track?.url,
-          })}
-          size={45}
-          mode="contained"
-          iconColor="#fff"
-          containerColor="#000"
-          onPress={async (e) => {
-            e.stopPropagation();
+        {songUrl && (
+          <IconButton
+            style={{ position: "absolute", bottom: 2, right: 2, zIndex: 50 }}
+            icon={getPlayerIcon({
+              bufferingDuringPlay,
+              playing,
+              isCurrent: isActiveTrack,
+            })}
+            size={45}
+            mode="contained"
+            iconColor="#fff"
+            containerColor="#000"
+            onPress={async (e) => {
+              e.stopPropagation();
 
-            const thisTrack =
-              pb.files.getUrl(props.song, props.song.file ?? "") === track?.url;
-            if (!thisTrack) {
-              TrackPlayer.add(
-                [{ url: pb.files.getUrl(props.song, props.song.file ?? ""), title: props.song.name }],
-                ((await TrackPlayer.getActiveTrackIndex()) ?? -1) + 1
-              );
-              TrackPlayer.play();
-              TrackPlayer.skipToNext();
-              return;
-            }
+              const thisTrack = isActiveTrack;
+              if (!thisTrack) {
+                TrackPlayer.add(
+                  [
+                    {
+                      url: pb.files.getUrl(props.song, props.song.file ?? ""),
+                      title: props.song.name,
+                    },
+                  ],
+                  ((await TrackPlayer.getActiveTrackIndex()) ?? -1) + 1
+                );
+                TrackPlayer.play();
+                TrackPlayer.skipToNext();
+                return;
+              }
 
-            if (bufferingDuringPlay) return null;
+              if (bufferingDuringPlay) return null;
 
-            return playing ? TrackPlayer.pause() : TrackPlayer.play();
-          }}
-        />
+              return playing ? TrackPlayer.pause() : TrackPlayer.play();
+            }}
+          />
+        )}
       </View>
       <SectionCardTitle>{props.title}</SectionCardTitle>
     </SectionCardContainer>
